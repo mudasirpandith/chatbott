a8863931169ad41ebb892b2f9aaadaa36865a1a4
Mudasir Pandith <78873815+mudasirpandith@users.noreply.github.com>
2024-01-23 19:09:01 +0530

    added seo

--- app/id/[chatId]/page.tsx
+++ app/id/[chatId]/page.tsx
@@ -1,13 +1,33 @@
 import { getChatById } from "@/libs/chat-services";
 import ChatCard from "./_components/chatCard";
 import { Container, Text } from "@chakra-ui/react";
-import { NextSeo } from "next-seo";
+import Head from "next/head";
+import { Metadata } from "next";
+export async function generateMetadata({
+  params,
+}: {
+  params: { chatId: string };
+}): Promise<Metadata> {
+  const seo = await getChatById(params.chatId);
+
+  return {
+    title: `ChatBott- Conversation between ${seo?.user?.name} and ChatBott`,
+    description: `Conversation : ${seo?.messages[0].content}`,
+    openGraph: {
+      images: [seo?.user.profileImage as string],
+    },
+  };
+}
+
 export default async function Page({ params }: { params: { chatId: string } }) {
   const chatId = params.chatId;
   const chat = await getChatById(chatId);
 
   return (
     <>
+      <Head>
+        <title>jb jyg j jvhdfh</title>
+      </Head>
       <Container mx={"auto"} maxW={"3xl"}>
         {chat?.messages
           ?.slice()
