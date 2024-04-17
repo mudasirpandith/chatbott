d9a18c0c611fd69b557e71ab3b6ac483698f9c03
mudasirpandith <mudasirpandith789@gmail.com>
2024-04-17 10:47:58 +0530

    renamed

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         textAlign={"center"}
       >
         <Heading fontWeight={400} fontSize={"7xl"} color={"Green"}>
-          ChatBot
+          ChatBott
         </Heading>
         <Text color={"gray"} fontWeight={"400"}>
           This advanced chatbot is created using the powerful Next.js framework
